faf5e733fef946b9d92a9b47ae192691e00b9e57
livintin <natig.abdulov@gmail.com>
2024-08-02 13:26:55 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = ('name', 1, True, 1.2)
+print(immutable_var)
+immutable_var[0] = 'firstname' #объект "кортеж" не поддерживает назначение элементов
+mutable_list = ['yang', 1, 1.2, False]
+mutable_list[0] = 'old'
+print(mutable_list)
