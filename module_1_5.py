9076682fa0f39e87c3ce6d3fe7da79d3242c50f7
livintin <natig.abdulov@gmail.com>
2024-08-02 13:27:46 +0700

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,6 @@
 immutable_var = ('name', 1, True, 1.2)
 print(immutable_var)
-immutable_var[0] = 'firstname' #объект "кортеж" не поддерживает назначение элементов
+immutable_var[0] = 'firstname' #объект "кортеж" не поддерживает назначение(изменение) элементов
 mutable_list = ['yang', 1, 1.2, False]
 mutable_list[0] = 'old'
 print(mutable_list)
